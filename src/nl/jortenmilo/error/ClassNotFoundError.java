820f305c675e5e338fcedc92f03e2c0df2063a4f
JortHub <jortvandriel@hotmail.com>
2017-03-31 15:35:39 +0200

    Update 0.1.2_10b
    - Changing the error system

--- src/nl/jortenmilo/error/ClassNotFoundError.java
+++ src/nl/jortenmilo/error/ClassNotFoundError.java
@@ -2,7 +2,6 @@ package nl.jortenmilo.error;
 
 import nl.jortenmilo.console.Console;
 import nl.jortenmilo.console.ConsoleUser;
-import nl.jortenmilo.event.EventHandler;
 import nl.jortenmilo.utils.defaults.SystemUtils;
 
 /**
@@ -21,18 +20,12 @@ public class ClassNotFoundError extends Error {
 	
 	@Override
 	public void print() {
-		Console.debug("ERROR [" + new SystemUtils().getTime() + "][ClassNotFound][" + value1 + ", " + value2 + "]");
+		Console.debug("ERROR [" + new SystemUtils().getTime() + "][ClassNotFoundError][" + value1 + ", " + value2 + "]");
 		
-		Console.println(ConsoleUser.Error, "ClassNotFound: The class '" + value1 + "' is not found!");
+		this.printError("ClassNotFoundError", "The class '" + value1 + "' is not found!");
 		Console.println(ConsoleUser.Error, "Caused by: " + value2);
-		Console.println(ConsoleUser.Error, "If you don't know what to do, please contact us at: goo.gl/1ROGMh.");
-		
-		ErrorThrownEvent event = new ErrorThrownEvent();
-		event.setError(this);
-		
-		for(EventHandler handler : getEventManager().getHandlers(event.getClass())) {
-			handler.execute(event);
-		}
+		this.printHelp();
+		this.event();
 	}
 
 }
--- src/nl/jortenmilo/error/ExistingSettingError.java
+++ src/nl/jortenmilo/error/ExistingSettingError.java
@@ -1,9 +1,5 @@
 package nl.jortenmilo.error;
 
-import nl.jortenmilo.console.Console;
-import nl.jortenmilo.console.ConsoleUser;
-import nl.jortenmilo.event.EventHandler;
-
 /**
  * This error is thrown when a settings is created that already exists.
  * @see Error
@@ -18,27 +14,10 @@ public class ExistingSettingError extends Error {
 	
 	@Override
 	public void print() {
-		Console.println(ConsoleUser.Error, "ExistingSettingError: The setting '" + value + "' already exists!");
-		
-		StackTraceElement[] es = Thread.currentThread().getStackTrace();
-		StackTraceElement[] es2 = new StackTraceElement[es.length-2];
-		
-		for(int i = 0; i < es2.length; i++) {
-			es2[i] = es[i+2];
-		}
-		
-		for(StackTraceElement e : es2) {
-			Console.println(ConsoleUser.Error, " at: " + e.getClassName() + "." + e.getMethodName() + " (Line: " + e.getLineNumber() + " in " + e.getFileName() + ")");
-		}
-		
-		Console.println(ConsoleUser.Error, "If you don't know what to do, please contact us at: goo.gl/1ROGMh.");
-		
-		ErrorThrownEvent event = new ErrorThrownEvent();
-		event.setError(this);
-		
-		for(EventHandler handler : getEventManager().getHandlers(event.getClass())) {
-			handler.execute(event);
-		}
+		this.printError("ExistingSettingError", "The setting '" + value + "' already exists!");
+		this.printStackTrace();
+		this.printHelp();
+		this.event();
 	}
 	
 }
